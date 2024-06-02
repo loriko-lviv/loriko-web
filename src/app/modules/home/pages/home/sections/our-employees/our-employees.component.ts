45228bb6c1bb5988cc1c4bcce150e783b47e2996
Denys Korotin <denys.korotin@apollo.ai>
2024-06-02 21:43:11 +0300

    Improved images

--- src/app/modules/home/pages/home/sections/our-employees/our-employees.component.ts
+++ src/app/modules/home/pages/home/sections/our-employees/our-employees.component.ts
@@ -16,21 +16,21 @@ export class OurEmployeesComponent {
       lastname: 'Раба',
       description: 'Лікар-отоларинголог. Стаж більше 10 років',
       router: ['/doctors', 'raba-bohdan'],
-      imageUrl: '/assets/l-kar-otorinolaringolog-back.png',
+      imageUrl: '/assets/l-kar-otorinolaringolog-back.webp',
     },
     {
       firstname: 'Галина Іванівна',
       lastname: 'Раба',
       description: 'Лікар-косметолог. Стаж більше 10 років',
       router: ['/doctors', 'raba-galia'],
-      imageUrl: '/assets/l-kar-kosmetolog.png',
+      imageUrl: '/assets/l-kar-kosmetolog.webp',
     },
     {
       firstname: 'Христина Вікторівна',
       lastname: 'Коротіна',
       description: 'Лікар-отоларинголог. Стаж більше 2 років',
       router: ['/doctors', 'korotina-khristina'],
-      imageUrl: '/assets/korotina_kh.png',
+      imageUrl: '/assets/korotina_kh.webp',
     }
   ]
 
